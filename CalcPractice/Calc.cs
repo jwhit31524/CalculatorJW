408675a17263245ce204072aca563da4389eec72
James <jwhitton316@gmail.com>
2020-06-10 11:51:24 -0400

    Add project files.

---
+++ CalcPractice/Calc.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalcPractice
+{
+    class Calc
+    {
+        public class Memento
+        {
+            public readonly decimal SavedState;
+
+            public Memento(decimal stateToSave)
+            {
+                SavedState = stateToSave;
+            }
+            public decimal GetSavedState()
+            {
+                return SavedState;
+            }
+        }
+
+        /*
+        
+        int result;
+        public Calculator(int i = 0)
+        {
+            result = 0;
+        }
+        public void SetResult(int i = 0)
+        {
+            this.result = 0;
+        }
+        public void Add(int x)
+        {
+            result += x;
+        }
+        public void Subtract(int x)
+        {
+            result -= x;
+        }
+        public int GetResult()
+        {
+            return result;
+        }
+        public Memento CreateMemento()
+        {
+            Memento memento = new Memento();
+            memento.SetState(result);
+            return memento;
+        }
+        public void SaveState(Memento memento)
+        {
+            result = memento.GetState();
+        }
+    }
+    
+
+
+private decimal runningTotal;
+
+    private decimal state;
+    // The class could also contain additional data that is not part of the
+    // state saved in the memento.
+
+    public void Set(decimal state)
+    {
+        Console.WriteLine("Calculator: Setting state to " + state);
+        this.state = state;
+    }
+
+    public Memento SaveToMemento()
+    {
+        Console.WriteLine("Calculator: Saving to Memento.");
+        return new Memento(state);
+    }
+
+    public void RestoreFromMemento(Memento memento)
+    {
+        state = memento.GetSavedState();
+        Console.WriteLine("Calculator: State after restoring from Memento: " + state);
+    }
+    */
+        private decimal output;
+        public decimal runningTotal;
+
+
+
+        public Calc()
+        {
+            //this.output = op;
+        }
+        public void SetOutput(decimal op)
+        {
+            this.output = op;
+        }
+        public decimal GetOutput()
+        {
+            return output;
+        }
+        public decimal AddNumbers(decimal enteredNumber1, decimal enteredNumber2)
+        {
+            //Console.WriteLine($"Your result: {enteredNumber1} + {enteredNumber2} = " + (enteredNumber1 + enteredNumber2));
+
+            output = enteredNumber1 + enteredNumber2;
+            runningTotal = runningTotal + output;
+
+
+            return output;
+
+        }
+
+        public decimal SubtractNumbers(decimal enteredNumber1, decimal enteredNumber2)
+        {
+
+            // Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+
+
+            output = enteredNumber1 - enteredNumber2;
+            runningTotal = runningTotal + output;
+
+            return output;
+
+        }
+
+
+        public decimal MultiplyNumbers(decimal enteredNumber1, decimal enteredNumber2)
+        {
+            //Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+            output = enteredNumber1 * enteredNumber2;
+            runningTotal = runningTotal + output;
+            return output;
+
+        }
+        public decimal DivideNumbers(decimal enteredNumber1, decimal enteredNumber2)
+        {
+            // Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+            output = enteredNumber1 / enteredNumber2;
+            runningTotal = runningTotal + output;
+            return output;
+
+        }
+        public void ExitCalculator()
+        {
+            Environment.Exit(0);
+
+        }
+
+
+        /*
+        public void UndoChanges()
+        {
+            //var _continue = true;
+            //“UNDO IS NOT AVAILABLE” and request the user to enter a command or calculation request.   
+            do
+            {
+                try
+                {
+                    s.Pop();
+
+                    // _continue = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Undo is not available");
+                    Console.WriteLine("You must enter a command or calculation request");
+
+                }
+            } while (s.Count > 0);
+
+
+        }
+        public void ClearNumbers()
+        {
+
+
+        }
+        public void ExitCalculator()
+        {
+            Environment.Exit(0);
+
+        }
+
+    }
+}
+
+        */
+        /*
+
+                s.Push("Bob");  // add Bob to the stack 
+
+
+
+                    s.Push("Amy");  // add Amy to the stack 
+
+
+
+                    String name = s.Peek();  // look at the top element in the stack
+
+                name = s.Pop();  // remove the top element in the stack 
+
+
+
+                    name = s.Pop();  //remove the top element in the stack 
+                    */
+
+    }
+}
+    
+
+
+
+
---
+++ CalcPractice/Caretaker.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace CalcPractice
+{
+    class Caretaker
+    {
+        private static bool EnterAgainPrompt()
+        {
+            Console.WriteLine("Enter Another Calculation? \n \n Type: 'Y' for yes or 'N' for no");
+            string answerToEnterAgain = (Console.ReadLine());
+            if (answerToEnterAgain.ToLower() != "y")
+            {
+                return false;
+            }
+            return true;
+        }
+        public static void Main(string[] args)
+        {
+            bool _continue = true;
+
+
+            while (_continue)
+            {
+               
+           /* Stack<Calculator.Memento> savedStates = new Stack<Calculator.Memento>();
+
+
+            Calculator calculator = new Calculator();
+            calculator.Set(1);
+            calculator.Set(2);
+            //savedStates.Push(calculator.SaveToMemento());
+            //calculator.Set(3);
+            // We can request multiple mementos, and choose which one to roll back to.
+            savedStates.Push(calculator.SaveToMemento());
+            calculator.Set(4);
+
+            calculator.RestoreFromMemento(savedStates.Peek());
+            */
+
+
+/*
+Calculator calculator = new Calculator();
+calculator.Push(5);
+calculator.Add(10);
+calculator.Subtract(10);
+Memento checkPoint = calculator.CreateMemento();
+calculator.Add(100);
+Console.WriteLine(“The value of the result variable is: “+calculator.GetResult());
+calculator.SaveState(checkPoint);
+Console.WriteLine(“The value of the result variable at first checkpoint is: “ +calculator.GetResult());
+Console.Read();
+*/
+
+Calc calculator = new Calc();
+
+
+
+Console.WriteLine("Please enter a number");
+decimal enteredNumber1 = decimal.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Please type the corresponding symbol of which operation you want to perform on the number: ");
+
+Console.WriteLine("\t+ - Add");
+Console.WriteLine("\t- - Subtract");
+Console.WriteLine("\t* - Multiply");
+Console.WriteLine("\t/ - Divide");
+// Console.WriteLine("\te - Equals");
+
+string enteredOperation = Console.ReadLine();
+
+Console.WriteLine("Please enter a second number for the operation");
+decimal enteredNumber2 = decimal.Parse(Console.ReadLine());
+
+
+// Use a switch statement to do the math.
+decimal localOutput = 0;
+if (enteredOperation == "+")
+{
+    localOutput = calculator.AddNumbers(enteredNumber1, enteredNumber2);
+}
+else if (enteredOperation == "-") {
+
+    localOutput = calculator.SubtractNumbers(enteredNumber1, enteredNumber2);
+}
+else if (enteredOperation == "*")
+{
+
+    localOutput = calculator.MultiplyNumbers(enteredNumber1, enteredNumber2);
+}
+else if (enteredOperation == "/")
+{
+
+    localOutput = calculator.DivideNumbers(enteredNumber1, enteredNumber2);
+}
+calculator.SetOutput(localOutput);
+Console.WriteLine();
+Console.WriteLine($"  Calculations: {enteredNumber1} {enteredOperation} {enteredNumber2} = {calculator.GetOutput()}");
+Console.WriteLine();
+Console.WriteLine($"Running Total : {calculator.runningTotal}");
+Console.WriteLine();
+_continue = EnterAgainPrompt();
+Console.WriteLine();
+}
+}
+}
+}
