3814dadd662a86ec6f1e283d990ca6056dedbeb9
James <jwhitton316@gmail.com>
2020-06-10 15:00:01 -0400

    Program is functioning with desired output

--- CalcPractice/Calc.cs
+++ CalcPractice/Calc.cs
@@ -6,83 +6,40 @@ namespace CalcPractice
 {
     class Calc
     {
+        private string state;
+        // The class could also contain additional data that is not part of the
+        // state saved in the memento.
+
+        public void Set(string state)
+        {
+            Console.WriteLine("Originator: Setting state to " + state);
+            this.state = state;
+        }
+
+        public Memento SaveToMemento()
+        {
+            Console.WriteLine("Originator: Saving to Memento.");
+            return new Memento(state);
+        }
+
+        public void RestoreFromMemento(Memento memento)
+        {
+            state = memento.SavedState;
+            Console.WriteLine("Originator: State after restoring from Memento: " + state);
+        }
+
         public class Memento
         {
-            public readonly decimal SavedState;
+            public readonly string SavedState;
 
-            public Memento(decimal stateToSave)
+            public Memento(string stateToSave)
             {
                 SavedState = stateToSave;
             }
-            public decimal GetSavedState()
-            {
-                return SavedState;
-            }
+           
         }
-
-        /*
-        
-        int result;
-        public Calculator(int i = 0)
-        {
-            result = 0;
-        }
-        public void SetResult(int i = 0)
-        {
-            this.result = 0;
-        }
-        public void Add(int x)
-        {
-            result += x;
-        }
-        public void Subtract(int x)
-        {
-            result -= x;
-        }
-        public int GetResult()
-        {
-            return result;
-        }
-        public Memento CreateMemento()
-        {
-            Memento memento = new Memento();
-            memento.SetState(result);
-            return memento;
-        }
-        public void SaveState(Memento memento)
-        {
-            result = memento.GetState();
-        }
-    }
-    
-
-
-private decimal runningTotal;
-
-    private decimal state;
-    // The class could also contain additional data that is not part of the
-    // state saved in the memento.
-
-    public void Set(decimal state)
-    {
-        Console.WriteLine("Calculator: Setting state to " + state);
-        this.state = state;
-    }
-
-    public Memento SaveToMemento()
-    {
-        Console.WriteLine("Calculator: Saving to Memento.");
-        return new Memento(state);
-    }
-
-    public void RestoreFromMemento(Memento memento)
-    {
-        state = memento.GetSavedState();
-        Console.WriteLine("Calculator: State after restoring from Memento: " + state);
-    }
-    */
         private decimal output;
-        public decimal runningTotal;
+        //public decimal runningTotal;
 
 
 
@@ -103,7 +60,7 @@ private decimal runningTotal;
             //Console.WriteLine($"Your result: {enteredNumber1} + {enteredNumber2} = " + (enteredNumber1 + enteredNumber2));
 
             output = enteredNumber1 + enteredNumber2;
-            runningTotal = runningTotal + output;
+            //  runningTotal = runningTotal + output;
 
 
             return output;
@@ -117,7 +74,7 @@ private decimal runningTotal;
 
 
             output = enteredNumber1 - enteredNumber2;
-            runningTotal = runningTotal + output;
+            // runningTotal = runningTotal + output;
 
             return output;
 
@@ -128,7 +85,7 @@ private decimal runningTotal;
         {
             //Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
             output = enteredNumber1 * enteredNumber2;
-            runningTotal = runningTotal + output;
+            // runningTotal = runningTotal + output;
             return output;
 
         }
@@ -136,7 +93,7 @@ private decimal runningTotal;
         {
             // Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
             output = enteredNumber1 / enteredNumber2;
-            runningTotal = runningTotal + output;
+            // runningTotal = runningTotal + output;
             return output;
 
         }
@@ -145,68 +102,269 @@ private decimal runningTotal;
             Environment.Exit(0);
 
         }
-
-
+    }
+}
         /*
-        public void UndoChanges()
+        private int result = 0;
+
+        public int Add(int a)
         {
-            //var _continue = true;
-            //“UNDO IS NOT AVAILABLE” and request the user to enter a command or calculation request.   
-            do
+            this.result += a;
+            return this.result;
+        }
+
+        public int Subtract(int a)
+        {
+            this.result -= a;
+            return this.result;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Result: " + this.result);
+        }
+
+        public CalculatorMemento CreateMemento()
+        {
+            CalculatorMemento calcMemento = new CalculatorMemento();
+            calcMemento.SetState(this.result);
+            return calcMemento;
+        }
+
+        public void SetMemento(CalculatorMemento memento)
+        {
+            this.result = memento.GetState();
+        }
+    }
+
+    public class CalculatorMemento
+    {
+        private int state;
+
+        public int GetState()
+        {
+            return this.state;
+        }
+
+        public void SetState(int state)
+        {
+            this.state = state;
+        }
+    }
+}
+*/
+/*  public class Memento
+  {
+      public readonly decimal SavedState;
+
+      public Memento(decimal stateToSave)
+      {
+          SavedState = stateToSave;
+      }
+      public decimal GetSavedState()
+      {
+          return SavedState;
+      }
+  }
+  public Memento CreateMemento()
+  {
+      Memento memento = new Memento();
+      memento.SetState(output);
+      return memento;
+  }
+  public void SaveState(Memento memento)
+  {
+      output = memento.GetState();
+  }
+  */
+/*
+
+int result;
+public Calculator(int i = 0)
+{
+    result = 0;
+}
+public void SetResult(int i = 0)
+{
+    this.result = 0;
+}
+public void Add(int x)
+{
+    result += x;
+}
+public void Subtract(int x)
+{
+    result -= x;
+}
+public int GetResult()
+{
+    return result;
+}
+public Memento CreateMemento()
+{
+    Memento memento = new Memento();
+    memento.SetState(result);
+    return memento;
+}
+public void SaveState(Memento memento)
+{
+    result = memento.GetState();
+}
+}
+
+
+
+private decimal runningTotal;
+
+private decimal state;
+// The class could also contain additional data that is not part of the
+// state saved in the memento.
+
+public void Set(decimal state)
+{
+Console.WriteLine("Calculator: Setting state to " + state);
+this.state = state;
+}
+
+public Memento SaveToMemento()
+{
+Console.WriteLine("Calculator: Saving to Memento.");
+return new Memento(state);
+}
+
+public void RestoreFromMemento(Memento memento)
+{
+state = memento.GetSavedState();
+Console.WriteLine("Calculator: State after restoring from Memento: " + state);
+}
+*/
+/*
+    private decimal output;
+    public decimal runningTotal;
+
+
+
+    public Calc()
+    {
+        //this.output = op;
+    }
+    public void SetOutput(decimal op)
+    {
+        this.output = op;
+    }
+    public decimal GetOutput()
+    {
+        return output;
+    }
+    public decimal AddNumbers(decimal enteredNumber1, decimal enteredNumber2)
+    {
+        //Console.WriteLine($"Your result: {enteredNumber1} + {enteredNumber2} = " + (enteredNumber1 + enteredNumber2));
+
+        output = enteredNumber1 + enteredNumber2;
+        runningTotal = runningTotal + output;
+
+
+        return output;
+
+    }
+
+    public decimal SubtractNumbers(decimal enteredNumber1, decimal enteredNumber2)
+    {
+
+        // Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+
+
+        output = enteredNumber1 - enteredNumber2;
+        runningTotal = runningTotal + output;
+
+        return output;
+
+    }
+
+
+    public decimal MultiplyNumbers(decimal enteredNumber1, decimal enteredNumber2)
+    {
+        //Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+        output = enteredNumber1 * enteredNumber2;
+        runningTotal = runningTotal + output;
+        return output;
+
+    }
+    public decimal DivideNumbers(decimal enteredNumber1, decimal enteredNumber2)
+    {
+        // Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+        output = enteredNumber1 / enteredNumber2;
+        runningTotal = runningTotal + output;
+        return output;
+
+    }
+    public void ExitCalculator()
+    {
+        Environment.Exit(0);
+
+    }
+
+
+    /*
+    public void UndoChanges()
+    {
+        //var _continue = true;
+        //“UNDO IS NOT AVAILABLE” and request the user to enter a command or calculation request.   
+        do
+        {
+            try
             {
-                try
-                {
-                    s.Pop();
+                s.Pop();
 
-                    // _continue = false;
-                }
-                catch
-                {
-                    Console.WriteLine("Undo is not available");
-                    Console.WriteLine("You must enter a command or calculation request");
+                // _continue = false;
+            }
+            catch
+            {
+                Console.WriteLine("Undo is not available");
+                Console.WriteLine("You must enter a command or calculation request");
 
-                }
-            } while (s.Count > 0);
+            }
+        } while (s.Count > 0);
 
-
-        }
-        public void ClearNumbers()
-        {
-
-
-        }
-        public void ExitCalculator()
-        {
-            Environment.Exit(0);
-
-        }
 
     }
-}
+    public void ClearNumbers()
+    {
 
-        */
-        /*
-
-                s.Push("Bob");  // add Bob to the stack 
-
-
-
-                    s.Push("Amy");  // add Amy to the stack 
-
-
-
-                    String name = s.Peek();  // look at the top element in the stack
-
-                name = s.Pop();  // remove the top element in the stack 
-
-
-
-                    name = s.Pop();  //remove the top element in the stack 
-                    */
 
     }
+    public void ExitCalculator()
+    {
+        Environment.Exit(0);
+
+    }
+
 }
-    
+}
+
+    */
+/*
+
+        s.Push("Bob");  // add Bob to the stack 
+
+
+
+            s.Push("Amy");  // add Amy to the stack 
+
+
+
+            String name = s.Peek();  // look at the top element in the stack
+
+        name = s.Pop();  // remove the top element in the stack 
+
+
+
+            name = s.Pop();  //remove the top element in the stack 
+            */
+
+
+
+
 
 
 
--- CalcPractice/Caretaker.cs
+++ CalcPractice/Caretaker.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CalcPractice
 {
@@ -18,24 +20,174 @@ namespace CalcPractice
         {
             bool _continue = true;
 
+            StringBuilder runtotal = new StringBuilder();
+
+            decimal rtSum = 0;
+
 
             while (_continue)
             {
+
+                /* List<Originator.Memento> savedStates = new List<Originator.Memento>();
+
+                 Originator originator = new Originator();
+                 originator.Set("State1");
+                 originator.Set("State2");
+                 savedStates.Add(originator.SaveToMemento());
+                 originator.Set("State3");
+                 // We can request multiple mementos, and choose which one to roll back to.
+                 savedStates.Add(originator.SaveToMemento());
+                 originator.Set("State4");
+
+                 originator.RestoreFromMemento(savedStates[1]);
+
+         */
+                Stack<Calc.Memento> savedStates = new Stack<Calc.Memento>();
+
+
+                Calc calculator = new Calc();
+
+
+
+                //savedStates.Push(calculator.SaveToMemento());
+                //calculator.Set(3);
+                // We can request multiple mementos, and choose which one to roll back to.
+                //savedStates.Push(calculator.SaveToMemento());
+                //calculator.Set(4);
+
+                //calculator.RestoreFromMemento(savedStates.Peek());
+
+
+
+                Console.WriteLine("Please enter a number");
+                decimal enteredNumber1 = decimal.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Please type the corresponding symbol of which operation you want to perform on the number: ");
+
+                Console.WriteLine("\t+ - Add");
+                Console.WriteLine("\t- - Subtract");
+                Console.WriteLine("\t* - Multiply");
+                Console.WriteLine("\t/ - Divide");
+                // Console.WriteLine("\te - Equals");
+
+                string enteredOperation = Console.ReadLine();
+
+                Console.WriteLine("Please enter a second number for the operation");
+                decimal enteredNumber2 = decimal.Parse(Console.ReadLine());
+
+
+                // Use a switch statement to do the math.
+                decimal localOutput = 0;
+                if (enteredOperation == "+")
+                {
+                    localOutput = calculator.AddNumbers(enteredNumber1, enteredNumber2);
+                }
+                else if (enteredOperation == "-")
+                {
+
+                    localOutput = calculator.SubtractNumbers(enteredNumber1, enteredNumber2);
+                }
+                else if (enteredOperation == "*")
+                {
+
+                    localOutput = calculator.MultiplyNumbers(enteredNumber1, enteredNumber2);
+                }
+                else if (enteredOperation == "/")
+                {
+
+                    localOutput = calculator.DivideNumbers(enteredNumber1, enteredNumber2);
+                }
+
+                rtSum = rtSum + calculator.GetOutput();
+                calculator.SetOutput(localOutput);
+                Console.WriteLine();
+                Console.WriteLine($"  Calculations: {enteredNumber1} {enteredOperation} {enteredNumber2} = {calculator.GetOutput()}");
+                calculator.Set($"{calculator.GetOutput()}");
+                savedStates.Push(calculator.SaveToMemento());
+                Console.WriteLine();
+                // Console.WriteLine($"Running Total : {calculator.runningTotal}");
+                Console.WriteLine();
+                //runtotal.Append(calculator.GetOutput().ToString());
+                runtotal.Append("+" + calculator.GetOutput().ToString());
+                // Console.WriteLine($"Running Total: {runtotal} + "+"");
+                Console.WriteLine($"Running Total: {rtSum}");
+                Console.WriteLine();
+                /*
+                Stack<MyClass> stacky = new Stack<MyClass>();
+
+                foreach (MyClass item in stacky)
+                {
+                    // this is as fast as you're going to get.
+                }
+                */
+               /* foreach (Calc.Memento calcSum in savedStates)
+                {
+                 
+                } */
+               // calculator.Set($"{calculator.GetOutput()}");
+               // savedStates.Push(calculator.SaveToMemento());
+                calculator.RestoreFromMemento(savedStates.Peek());
+                _continue = EnterAgainPrompt();
+                Console.WriteLine();
+
+
                
-           /* Stack<Calculator.Memento> savedStates = new Stack<Calculator.Memento>();
+            }
+        }
+    }
+}
+        /*
+        static void Main(string[] args)
+        {
+            Calc calc = new Calc();
+            calc.Add(4);        // result 4
+            calc.Add(3);        // result 7
+            calc.Subtract(1);   // result 6
+
+            CalculatorMemento checkPoint1 = calc.CreateMemento();
+            calc.Add(1);        // result 7
+            calc.Print();       // print 7
+
+            calc.SetMemento(checkPoint1);
+            calc.Print();       // result 6
+        }
+    }
+}
+*/
+/* private static bool EnterAgainPrompt()
+ {
+     Console.WriteLine("Enter Another Calculation? \n \n Type: 'Y' for yes or 'N' for no");
+     string answerToEnterAgain = (Console.ReadLine());
+     if (answerToEnterAgain.ToLower() != "y")
+     {
+         return false;
+     }
+     return true;
+ }
+ public static void Main(string[] args)
+ {
+     bool _continue = true;
 
 
-            Calculator calculator = new Calculator();
-            calculator.Set(1);
-            calculator.Set(2);
-            //savedStates.Push(calculator.SaveToMemento());
-            //calculator.Set(3);
-            // We can request multiple mementos, and choose which one to roll back to.
-            savedStates.Push(calculator.SaveToMemento());
-            calculator.Set(4);
+     while (_continue)
+     {
+     */
 
-            calculator.RestoreFromMemento(savedStates.Peek());
-            */
+/* Stack<Calculator.Memento> savedStates = new Stack<Calculator.Memento>();
+
+
+ Calculator calculator = new Calculator();
+ calculator.Set(1);
+ calculator.Set(2);
+ //savedStates.Push(calculator.SaveToMemento());
+ //calculator.Set(3);
+ // We can request multiple mementos, and choose which one to roll back to.
+ savedStates.Push(calculator.SaveToMemento());
+ calculator.Set(4);
+
+ calculator.RestoreFromMemento(savedStates.Peek());
+ */
 
 
 /*
@@ -51,6 +203,7 @@ Console.WriteLine(“The value of the result variable at first checkpoint is: �
 Console.Read();
 */
 
+/*
 Calc calculator = new Calc();
 
 
@@ -77,21 +230,21 @@ decimal enteredNumber2 = decimal.Parse(Console.ReadLine());
 decimal localOutput = 0;
 if (enteredOperation == "+")
 {
-    localOutput = calculator.AddNumbers(enteredNumber1, enteredNumber2);
+localOutput = calculator.AddNumbers(enteredNumber1, enteredNumber2);
 }
 else if (enteredOperation == "-") {
 
-    localOutput = calculator.SubtractNumbers(enteredNumber1, enteredNumber2);
+localOutput = calculator.SubtractNumbers(enteredNumber1, enteredNumber2);
 }
 else if (enteredOperation == "*")
 {
 
-    localOutput = calculator.MultiplyNumbers(enteredNumber1, enteredNumber2);
+localOutput = calculator.MultiplyNumbers(enteredNumber1, enteredNumber2);
 }
 else if (enteredOperation == "/")
 {
 
-    localOutput = calculator.DivideNumbers(enteredNumber1, enteredNumber2);
+localOutput = calculator.DivideNumbers(enteredNumber1, enteredNumber2);
 }
 calculator.SetOutput(localOutput);
 Console.WriteLine();
@@ -104,4 +257,5 @@ Console.WriteLine();
 }
 }
 }
-}
+*/
+
